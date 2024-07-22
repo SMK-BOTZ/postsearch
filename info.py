922b58d992b7083735f6c01747c5d96fa42cc5e8
Devensh22345 <kumardevensh0@gmail.com>
2024-07-23 01:15:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 API_ID       = 22207976
 API_HASH     = "5c0ad7c48a86afac87630ba28b42560d"
-BOT_TOKEN    = "7138784027:AAG1T1LW74a7AHk_vddjO2JRi-4sp3vz_WU"
+BOT_TOKEN    = "6863575303:AAEDlQFsmDt6c_uqwDAORiw1xKCswEpIF3U-4sp3vz_WU"
 SESSION      = "BQFINEQAhvMzMd0dfry2iNr_YZaThgze75jCjSLWe-WBpWCtNGfkkhUWMXxFg5cwa9MfLeCPY-194euigWihfs-wazmmqwOobVsJiQY_T3dC_0fVilV2UWc8EyKD1WvFG0EMp9fb4KdodAE08KwekuALIE2QcRcPgD99db-LqOU4ypAKb1KPwRi9ephDOtQ5ooG9DPGwAkM6M5AhtOpWhzBkOjPBmQi5Z2KfE-LHrE5ScOe972etDz8TfLYGr2v3BTOTV-uYx48MoWoirEg_X8T5ykVO2NXN44BSR8tJantXdQGgCN9TvuBL_ztaO5ymlZzzkGkzrKyk0N7ncEKeuw3EUmF-GAAAAAFVDeYoAA"
 DATABASE_URI = "mongodb+srv://Autofilterbot:Autofilterbot@autofilterbot.evsptxd.mongodb.net/?retryWrites=true&w=majority&appName=Autofilterbot"
 LOG_CHANNEL  = -1001942398843
