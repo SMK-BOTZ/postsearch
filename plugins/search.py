0458743f438f61278145ab7b461f15af1fa389ae
Devensh22345 <kumardevensh0@gmail.com>
2024-07-23 01:06:34 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -1,104 +1,93 @@
 import asyncio
 from info import *
 from utils import *
-from time import time
+from time import time 
 from client import User
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-# Set a limit on the number of messages to forward at once
-FORWARD_LIMIT = 10
+from pyrogram import Client, filters 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
 
 @Client.on_message(filters.text & filters.group & filters.incoming & ~filters.command(["verify", "connect", "id"]))
 async def search(bot, message):
     f_sub = await force_sub(bot, message)
-    if f_sub == False:
-        return
-    
+    if f_sub==False:
+       return     
     channels = (await get_group(message.chat.id))["channels"]
-    if not channels:
-        return
-
+    if bool(channels)==False:
+       return     
     if message.text.startswith("/"):
-        return
-
-    query = message.text
-    forward_count = 0  # Counter to limit the number of forwards
-
+       return    
+    query   = message.text 
+    head    = "<u>Here is the results 👇\n\n </u> <b><I>👇</I></b>\n\n"
+    results = ""
     try:
-        for channel in channels:
-            async for msg in User.search_messages(chat_id=channel, query=query):
-                if forward_count < FORWARD_LIMIT:  # Check the limit before forwarding
-                    await msg.forward(message.chat.id)
-                    forward_count += 1
-                else:
-                    break
+       for channel in channels:
+           async for msg in User.search_messages(chat_id=channel, query=query):
+               name = (msg.text or msg.caption).split("\n")[0]
+               if name in results:
+                  continue 
+               results += f"<b><I>👉 {name}\n🔗 {msg.link}</I></b>\n\n"                                                      
+       if bool(results)==False:
+          movies = await search_imdb(query)
+          buttons = []
+          for movie in movies: 
+              buttons.append([InlineKeyboardButton(movie['title'], callback_data=f"recheck_{movie['id']}")])
+          msg = await message.reply_photo(photo="https://graph.org/file/20f2a42c266a15b3118c8.jpg",
+                                          caption="<b><I>I Couldn't find anything related to Your Query😕.\nDid you mean any of these?</I></b>", 
+                                          reply_markup=InlineKeyboardMarkup(buttons))
+       else:
+          msg = await message.reply_text(text=head+results, disable_web_page_preview=True)
+       _time = (int(time()) + (15*60))
+       await save_dlt_message(msg, _time)
+    except:
+       pass
+       
 
-        if forward_count == 0:
-            # If no results found, provide suggestions
-            movies = await search_imdb(query)
-            buttons = []
-            for movie in movies:
-                buttons.append([InlineKeyboardButton(movie['title'], callback_data=f"recheck_{movie['id']}")])
-
-            await message.reply_photo(
-                photo="https://graph.org/file/20f2a42c266a15b3118c8.jpg",
-                caption="<b><I>I couldn't find anything related to your query 😕.\nDid you mean any of these?</I></b>",
-                reply_markup=InlineKeyboardMarkup(buttons)
-            )
-    except Exception as e:
-        await message.reply_text(f"❌ Error: {e}")
 
 @Client.on_callback_query(filters.regex(r"^recheck"))
-async def recheck(bot, update):
+async def recheck(bot, update): regards 
     clicked = update.from_user.id
-    try:
-        typed = update.message.reply_to_message.from_user.id
+    try:      
+       typed = update.message.reply_to_message.from_user.id
     except:
-        return await update.message.delete()
-
+       return await update.message.delete(2)       
     if clicked != typed:
-        return await update.answer("That's not for you! 👀", show_alert=True)
+       return await update.answer("That's not for you! 👀", show_alert=True)
 
-    await update.message.edit("Searching..💥")
-    id = update.data.split("_")[-1]
-    query = await search_imdb(id)
+    m=await update.message.edit("Searching..💥")
+    id      = update.data.split("_")[-1]
+    query   = await search_imdb(id)
     channels = (await get_group(update.message.chat.id))["channels"]
-    forward_count = 0
-
+    head    = "<u>I Have Searched Movie With Wrong Spelling But Take care next time 👇\n\n.</u> <b><I>👇</I></b>\n\n"
+    results = ""
     try:
-        for channel in channels:
-            async for msg in User.search_messages(chat_id=channel, query=query):
-                if forward_count < FORWARD_LIMIT:  # Check the limit before forwarding
-                    await msg.forward(update.message.chat.id)
-                    forward_count += 1
-                else:
-                    break
-
-        if forward_count == 0:
-            return await update.message.edit(
-                "Still no results found! Please request to group admin",
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🎯 Request To Admin 🎯", callback_data=f"request_{id}")]])
-            )
+       for channel in channels:
+           async for msg in User.search_messages(chat_id=channel, query=query):
+               name = (msg.text or msg.caption).split("\n")[0]
+               if name in results:
+                  continue 
+               results += f"<b><I>♻️🍿 {name}</I></b>\n\n🔗 {msg.link}</I></b>\n\n"
+       if bool(results)==False:          
+          return await update.message.edit("Still no results found! Please Request To Group Admin", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🎯 Request To Admin 🎯", callback_data=f"request_{id}")]]))
+       await update.message.edit(text=head+results, disable_web_page_preview=True)
     except Exception as e:
-        await update.message.edit(f"❌ Error: {e}")
+       await update.message.edit(f"❌ Error: `{e}`")
+
 
 @Client.on_callback_query(filters.regex(r"^request"))
 async def request(bot, update):
     clicked = update.from_user.id
-    try:
-        typed = update.message.reply_to_message.from_user.id
+    try:      
+       typed = update.message.reply_to_message.from_user.id
     except:
-        return await update.message.delete()
-
+       return await update.message.delete()       
     if clicked != typed:
-        return await update.answer("That's not for you! 👀", show_alert=True)
+       return await update.answer("That's not for you! 👀", show_alert=True)
 
     admin = (await get_group(update.message.chat.id))["user_id"]
-    id = update.data.split("_")[1]
-    name = await search_imdb(id)
-    url = "https://www.imdb.com/title/tt" + id
-    text = f"#RequestFromYourGroup\n\nName: {name}\nIMDb: {url}"
+    id    = update.data.split("_")[1]
+    name  = await search_imdb(id)
+    url   = "https://www.imdb.com/title/tt"+id
+    text  = f"#RequestFromYourGroup\n\nName: {name}\nIMDb: {url}"
     await bot.send_message(chat_id=admin, text=text, disable_web_page_preview=True)
     await update.answer("✅ Request Sent To Admin", show_alert=True)
     await update.message.delete(60)
