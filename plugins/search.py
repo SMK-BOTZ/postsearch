60dd14d0e44c9a479864bc496c01f1f57ef04be0
Devensh22345 <kumardevensh0@gmail.com>
2024-07-24 13:10:28 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -17,18 +17,57 @@ async def search(bot, message):
     if message.text.startswith("/"):
        return    
     query   = message.text 
-    head    = "<u>Here is the results 👇\n\n👇</u> <b><I>👇</I></b>\n\n"
-    results = ""
+    head    = "<u>Here is the result 👇</u>"
     try:
        for channel in channels:
            async for msg in User.search_messages(chat_id=channel, query=query):
-               name = (msg.text or msg.caption).split("\n")[0]
-               if name in results:
-                  continue 
-               results += f"<b><I>👉 {name}\n🔗 {msg.link}</I></b>\n\n"                                                     
-        
+               # Forward the message instead of sending a link
+               await msg.forward(message.chat.id)
+               break  # Stop after forwarding the first matching message
+    except Exception as e:
+       await message.reply_text(f"❌ Error: {e}")
+
+
+@Client.on_callback_query(filters.regex(r"^recheck"))
+async def recheck(bot, update):
+    clicked = update.from_user.id
+    try:      
+       typed = update.message.reply_to_message.from_user.id
     except:
-       pass
-       
+       return await update.message.delete(2)       
+    if clicked != typed:
+       return await update.answer("That's not for you! 👀", show_alert=True)
+
+    m=await update.message.edit("Searching..💥")
+    id      = update.data.split("_")[-1]
+    query   = await search_imdb(id)
+    channels = (await get_group(update.message.chat.id))["channels"]
+    head    = "<u>I Have Searched With Wrong Spelling But Take care next time 👇 </u>"
+    try:
+       for channel in channels:
+           async for msg in User.search_messages(chat_id=channel, query=query):
+               # Forward the message instead of sending a link
+               await msg.forward(update.message.chat.id)
+               break  # Stop after forwarding the first matching message
+    except Exception as e:
+       await update.message.edit(f"❌ Error: {e}")
 
 
+@Client.on_callback_query(filters.regex(r"^request"))
+async def request(bot, update):
+    clicked = update.from_user.id
+    try:      
+       typed = update.message.reply_to_message.from_user.id
+    except:
+       return await update.message.delete()       
+    if clicked != typed:
+       return await update.answer("That's not for you! 👀", show_alert=True)
+
+    admin = (await get_group(update.message.chat.id))["user_id"]
+    id    = update.data.split("_")[1]
+    name  = await search_imdb(id)
+    url   = "https://www.imdb.com/title/tt"+id
+    text  = f"#RequestFromYourGroup\n\nName: {name}\nIMDb: {url}"
+    await bot.send_message(chat_id=admin, text=text, disable_web_page_preview=True)
+    await update.answer("✅ Request Sent To Admin", show_alert=True)
+    await update.message.delete(60)
