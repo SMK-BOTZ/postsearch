9f1a746ed51558970144b9550a870f6994af5271
Devensh22345 <kumardevensh0@gmail.com>
2024-07-28 03:19:25 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -4,26 +4,32 @@ class script(object):
 I am **Find Post Bot**.I am best Channel Link Search Bot! 
 I Will filter your channel posts automatically and send it in your group chat when Someone search it."""
 
-    HELP = """To Use me In A Group
+    HELP = """<b>To Use me In A Group
 
 - Add me in your group & channel with all permissions. 
-- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @DK_ROBOTS.
+
+- Add My Assistant in your group & channel with all permissions [Remain anonymous permission Required in Group] . Assistant Id - @Miss_Naruto
+
+- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @DK_x_BOT.
+
 - After verification send /connect YourChannelID
+
 - Example : /connect -100xxxxxxxxxx
+
 - Done ✅. <b><I>Enjoy 💜❤</I></b> )
 
 Remove a Channel with - /disconnect-100xxxxxxxxxxx
 this will help you to remove a indexed channel from your group.
 
-Get connected channels list with - /connections"""
+Get connected channels list with - /connections</b>"""
 
-    ABOUT = """Powered By @DK_ROBOTS
+    ABOUT = """<b>Powered By @DK_x_BOT
 
 ✯ Mʏ Nᴀᴍᴇ:  {}
 ✯ Cʀᴇᴀᴛᴏʀ: <a href='tg://user?id=6872968794'>🔥︵L͙I͙G͙H͙T͙ ✿ Y͙A͙G͙A͙M͙I͙︵🔥</a>
-✯ SUPPORT GROUP: <a href='https://t.me/DK_ROBOTS_GROUP'>@DK_ROBOTS_GROUO</a>
-✯ CHANNEL: <a href='https://cloud.mongodb.com/'>@DK_ROBOTS</a>
-✯ DONATE: : <a href='tg://user?id=6872968794'>BUY A COFEE</a>"""
+✯ SUPPORT: <a href='https://t.me/DKxBOT_GROUP'>@DKxBOT_GROUO</a>
+✯ CHANNEL: <a href='https://t.me/DK_x_BOT'>@DK_x_BOT</a>
+✯ DONATE: : <a href='tg://user?id=6872968794'>BUY A COFEE</a></b>"""
 
     STATS = """My Status 💫
 
