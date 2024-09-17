b9d7a8f5ecca06b63af247db97aa4be418b39359
Devensh22345 <kumardevensh0@gmail.com>
2024-09-17 20:22:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "@CyNiTeBaCkUp"
+API_ID       = 22207976
+API_HASH     = "5c0ad7c48a86afac87630ba28b42560d"
+BOT_TOKEN    = "6863575303:AAHxr_BoTljbkzi4xwVJJKQkGuMudHc1ijM"
+SESSION      = "BQAncJb047wcFSUie2bbX4nDIUqVdyowoioe4e0Jd71nXMS4Sxzr2GCQr6fjn_Eo_SXgvQAgevOUgVmlqr5pQnreYGgoYuEFFlEj5jx9rFoVwgpIeN-fQbTmDuRf8od5Qkw0qMe6OxmDsdO4906f7CoRnMSweZxjQhiOr7PClarkoGJfzahPgIFnmyNrUPh4wzy0i_EjS2-SJhy3ZZbPqxC4sjD0EJ38lTYro4PYe6xMnplopkcPi58eIinrRKVsHlw3s2z0WoJJkKn23QopP4yg96KfNQyHROy7teEduPpBQHwHPJXHFgx5xaQYrS8iVBLAXcozvPlqWD_rInseH64fAAAAAVUN5igA"
+DATABASE_URI = "mongodb+srv://Autofilterbot:Autofilterbot@autofilterbot.evsptxd.mongodb.net/?retryWrites=true&w=majority&appName=Autofilterbot"
+LOG_CHANNEL  = -1001942398843
+ADMIN        = 6872968794
+CHANNEL      = "@DK_ANIMES"
