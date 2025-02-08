afdd2da7c67acaf56e889249d6743304b8a7dfdb
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2025-02-08 15:39:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,6 +2,6 @@ API_ID       = 22207976
 API_HASH     = "5c0ad7c48a86afac87630ba28b42560d"
 BOT_TOKEN    = "6863575303:AAHxr_BoTljbkzi4xwVJJKQkGuMudHc1ijM"
 SESSION      = "BQAncJb047wcFSUie2bbX4nDIUqVdyowoioe4e0Jd71nXMS4Sxzr2GCQr6fjn_Eo_SXgvQAgevOUgVmlqr5pQnreYGgoYuEFFlEj5jx9rFoVwgpIeN-fQbTmDuRf8od5Qkw0qMe6OxmDsdO4906f7CoRnMSweZxjQhiOr7PClarkoGJfzahPgIFnmyNrUPh4wzy0i_EjS2-SJhy3ZZbPqxC4sjD0EJ38lTYro4PYe6xMnplopkcPi58eIinrRKVsHlw3s2z0WoJJkKn23QopP4yg96KfNQyHROy7teEduPpBQHwHPJXHFgx5xaQYrS8iVBLAXcozvPlqWD_rInseH64fAAAAAVUN5igA"
-DATABASE_URI = "mongodb+srv://Clonefilestore2:Clonefilestore2@cluster0.qmizaj2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_URI = "mongodb+srv://Devensh:Devensh@cluster0.pe664.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = -1001942398843
 ADMIN        = 6872968794
