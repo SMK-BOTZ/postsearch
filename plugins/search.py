14ba1360e7c5b6587c7afbf4ebf11e76cf3ea56e
Devensh22345 <kumardevensh0@gmail.com>
2025-02-14 17:52:29 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -26,27 +26,3 @@ async def search(bot, message):
     except :
        pass
 
-@Client.on_callback_query(filters.regex(r"^recheck"))
-async def recheck(bot, update):
-    clicked = update.from_user.id
-    try:      
-       typed = update.message.reply_to_message.from_user.id
-    except:
-       return await update.message.delete(2)       
-    if clicked != typed:
-       return await update.answer("That's not for you! 👀", show_alert=True)
-
-    m=await update.message.edit("Searching..💥")
-    id      = update.data.split("_")[-1]
-    query   = await search_imdb(id)
-    channels = (await get_group(update.message.chat.id))["channels"]
-    head    = "<u>I Have Searched With Wrong Spelling But Take care next time 👇 </u>"
-    try:
-       for channel in channels:
-           async for msg in User.search_messages(chat_id=channel, query=query):
-               # Forward the message instead of sending a link
-               await msg.forward(update.message.chat.id)
-               break  # Stop after forwarding the first matching message
-    except :
-        pass
-
