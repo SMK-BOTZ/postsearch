4bdec18333719cbfefe483290892e98734636d6c
Devensh22345 <kumardevensh0@gmail.com>
2025-02-24 17:09:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
-API_ID       = 22207976
+API_ID       = "22207976"
 API_HASH     = "5c0ad7c48a86afac87630ba28b42560d"
 BOT_TOKEN    = "6863575303:AAHxr_BoTljbkzi4xwVJJKQkGuMudHc1ijM"
 SESSION      = "BQG_NJ0AoXQqtfXg3fbXYC35iyO20P2s8Hbu6kcUXERH4v_y2WLk2h5vLbIEuu_WteDrIOUifjlmvK9o2H4JcWsh7qfrPNj5dp8WeuIHPGpg9-w8bU_raqV1FNV5qkLYMqbETmT_lx0zWTVwACaqyvD2dpul0rjmMTZiBWg8r5Gc3vPClLz5bYo6Vnpz_0rd93ybq38A2hWwCjc3spPdeeu2z9Vknnk8FMvckIXClF7bkUjD9MG-aW0YrhVbsvoJPPpOfa5XEBJSLIC3ABAtR0FRKKBECjSaKYer9qJxzndTkLp6HWge7CU1LyAERyMWw6v73MgzjtpqTDxpW7jE4_PXfKPNtgAAAAFVDeYoAA"
 DATABASE_URI = "mongodb+srv://Devensh:Devensh@cluster0.pe664.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-LOG_CHANNEL  = -1001942398843
-ADMIN        = 6872968794
+LOG_CHANNEL  = "-1001942398843"
+ADMIN        = "6872968794"
